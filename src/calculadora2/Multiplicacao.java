187c19a18d4552798ad061ccfbbc40189f3b3f5c
ronaldo <ronaldo.jrk@gmail.com>
2019-03-26 14:23:09 -0300

    a multi

---
+++ src/calculadora2/Multiplicacao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora2;
+
+/**
+ *
+ * @author aluno
+ */
+public class Multiplicacao {
+    public int mult(int a, int b){
+        return a * b;
+    }
+    
+}
