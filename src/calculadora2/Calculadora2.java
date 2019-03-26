70edc113b7c1d812aca33653fda7f62330a0bee0
Matheus <theusmoraes@gmail.com>
2019-03-26 14:08:58 -0300

    Criando uma nova soma

--- src/calculadora2/Calculadora2.java
+++ src/calculadora2/Calculadora2.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculadora2;
-
-/**
- *
- * @author aluno
- */
+import java.util.Scanner;
 public class Calculadora2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String x, y;
+        String oper;
+        System.out.println("10 + 5 Enter!    Insira 0 a qualquer momento para sair");
+        do{
+            System.out.println("\nCALCULADORA\n");
+            x = input.next();
+            if(x.equals("0")){
+                break;//Calculadora para se recebe 0
+            }
+            oper = input.next();
+            y = input.next();
+            if(y.equals("0")){
+                break;//Calculadora para se recebe 0
+            }
+            switch (oper){
+                case "+":
+                break;
+                case "-":
+                break;
+                case "*":
+                break;
+                case "/":
+                break;
+                default:
+                break;
+            }
+         }while(!x.equals("0"));
     }
-    
 }
