c37afcbbddc77871e9170490485a282b76d104f2
lara <lara.ferrer001@gmail.com>
2019-03-28 16:55:37 -0300

    detalhes

--- src/calculadora2/Sub.java
+++ src/calculadora2/Sub.java
@@ -4,7 +4,8 @@ package calculadora2;
 
 public class Sub {
     public double subtraindo(double a, double b){
-        double sub= a-b;
+        double sub;
+        sub=a-b;
         return sub;
     }
 }
