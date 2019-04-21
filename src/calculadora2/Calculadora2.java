e10f6e7559805440b6f2ca1f1e7aff1c84a4337e
ViniciusDantz <vinicius.d.paranhos@gmail.com>
2019-04-21 15:06:07 -0300

    verify caracters pronto

--- src/calculadora2/Calculadora2.java
+++ src/calculadora2/Calculadora2.java
@@ -5,7 +5,7 @@ public class Calculadora2 {
         /*Método que recebe um dos Strings de números, transforma em um array de caracteres, 
         verifica se existe alguma vírgula nele, se existir ele troca por um ponto, evitando o
         erro "java.lang.NumberFormatException" e retornando a string correta do número*/
-        char todouble[] = numero.toCharArray();
+        char todouble[] = numero.toCharArray(); 
             for (int i = 0; i < todouble.length; i++) {
                 if(todouble[i] == ','){
                     todouble[i] = '.';
@@ -13,6 +13,19 @@ public class Calculadora2 {
             }
             numero = String.valueOf(todouble);
             return numero;
+    }
+    public static boolean verifyCharacters(String caracter){
+        /*Método que recebe um dos Strings de números, trata os dados contidos nela
+        lendo posição por posição e verificando se o UNICODE retornado pela posição
+        é um número, se não for ele retorna false e o main retorna a mensagem de "apenas
+        números. O UNICODE funciona de forma a atribuir um número, aqui em decimal, para cada
+        símbolo, os números de 0 a 9 tem o unicode entre 48 e 57, tudo que não for isso não entra na calculadora*/
+        for (int i = 0; i < caracter.length(); i++) {
+            if(caracter.codePointAt(i) != 46 && caracter.codePointAt(i) < 48 || caracter.codePointAt(i) > 57){
+                return false;
+            }
+        }
+        return true;
     }
     public static void main(String[] args){
         Soma soma = new Soma();
@@ -34,11 +47,19 @@ public class Calculadora2 {
             oper = input.next();
             oper = oper.toLowerCase();
             if ("v".equals(oper)){
-                System.out.println(x + " " + oper + " = " + Raiz.sqrt(Double.parseDouble(x)));
-                break;
+                if(!verifyCharacters(x)){
+                    System.out.println("Apenas números são permitidos neste campo!");
+                }else{
+                    System.out.println(x + " " + oper + " = " + Raiz.sqrt(Double.parseDouble(x)));
+                }
+                continue;//crie uma nova interação do loop
             }
             y = input.next();
             y = verifyComma(y);
+            if(!verifyCharacters(x) || !verifyCharacters(y)){
+                System.out.println("Apenas números são permitidos neste campo!");
+                continue;//crie uma nova interação do loop
+            }
             if(y.equals("0")){
                 break;//Calculadora break o do-while se receber 0
             }
@@ -62,6 +83,6 @@ public class Calculadora2 {
                     System.out.println("Operador não reconhecido!");
                 break;
             }
-         }while(!x.equals("0"));
+        }while(!x.equals("0"));
     }
 }
